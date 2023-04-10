d85bef4475e4095bca3aa3724c0ef8593fbf0eb5
«YouJhin» <palevich86@mail.ru>
2023-04-10 12:25:06 +0300

    del 'Tests' name :scissors:

--- Exceptions/HW1/Tsk3And4/Main.java
+++ Exceptions/HW1/Tsk3And4/Main.java
@@ -4,9 +4,9 @@ class Main {
     public static void main(String[] args) {
         float[] firstArray = Exceptions.HW1.Tsk3And4.FillArraysTs3.rndFills(10);
         float[] secondArray = Exceptions.HW1.Tsk3And4.FillArraysTs3.rndFills(10);
-        float[] testArr = null;
-        float[] testArrZero = {1,2,3,0};
-        float[] testArrZero2 = {1,2,3,4};
+        float[] nullArr = null;
+        float[] arrayWithZero = {1,2,3,0,5,6,7,8,9,10};
+
 
         //System.out.println(Arrays.toString(firstArray));
         // 3-е задание
